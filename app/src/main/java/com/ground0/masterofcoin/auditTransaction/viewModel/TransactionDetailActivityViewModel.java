2b5de01da481c376d8e26b179b9674213ccf3bfb
00-00-00 <nexusa8@gmail.com>
2016-12-24 02:12:34 +0530

    1. view model field bindings activity detail

--- app/src/main/java/com/ground0/masterofcoin/auditTransaction/viewModel/TransactionDetailActivityViewModel.java
+++ app/src/main/java/com/ground0/masterofcoin/auditTransaction/viewModel/TransactionDetailActivityViewModel.java
@@ -1,16 +1,20 @@
 package com.ground0.masterofcoin.auditTransaction.viewModel;
 
 import android.databinding.ObservableField;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import com.ground0.masterofcoin.auditTransaction.activity.TransactionDetailActivity;
 import com.ground0.masterofcoin.core.baseViewModel.BaseActivityViewModel;
 import com.ground0.masterofcoin.core.event.ViewDetail;
+import com.ground0.masterofcoin.core.util.ResourceUtil;
 import com.ground0.model.Expense;
 import com.ground0.model.TransactionObject;
 import com.ground0.repository.repository.UserRepository;
 import com.ground0.repository.repository.UserRepositoryImpl;
 import java.util.List;
 import org.apache.commons.lang3.StringUtils;
+import org.threeten.bp.format.DateTimeFormatter;
 import rx.android.schedulers.AndroidSchedulers;
 
 /**
@@ -26,6 +30,12 @@ public class TransactionDetailActivityViewModel
   Expense expense;
   ObservableField<Boolean> isFraud = new ObservableField<>(false);
   ObservableField<Boolean> isVerified = new ObservableField<>(false);
+  Drawable category;
+  int state;
+  String time;
+
+  private static final DateTimeFormatter DATE_TIME_FORMATTER =
+      DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss");
 
   @Override public void afterRegister() {
     super.afterRegister();
@@ -69,6 +79,40 @@ public class TransactionDetailActivityViewModel
   private void initFields() {
     isFraud.set("fraud".equals(expense.getState()));
     isVerified.set("verified".equals(expense.getState()));
+    category =
+        ContextCompat.getDrawable(getActivity(), ResourceUtil.getDrawable(expense.getCategory()));
+    state = ContextCompat.getColor(getActivity(), ResourceUtil.getColor(expense.getState()));
+    time = DATE_TIME_FORMATTER.format(expense.getTime());
+  }
+
+  private void saveData() {
+    getCompositeSubscription().add(userRepository.updateTransactions(transactionObject)
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(getSubscriptionBuilder().builder().onNext(value -> {
+          getActivity().showToast("Successfully Updated");
+          getActivity().finish();
+        }).onError(e -> {
+          String errorMessage = "Something went wrong";
+          if (e instanceof Throwable) {
+            errorMessage = StringUtils.isNotBlank(e.getMessage()) ? e.getMessage() : errorMessage;
+          }
+          getActivity().showError(errorMessage);
+        }).build()));
+  }
+
+  public void fetchData() {
+    getCompositeSubscription().add(userRepository.getTransactions()
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(getSubscriptionBuilder().builder().onNext(value -> {
+          transactionObject = (TransactionObject) value;
+          updateExpense();
+        }).onError(error -> {
+          error.printStackTrace();
+        }).setUnsubscribeOnComplete(true).build()));
+  }
+
+  public void retry() {
+    saveData();
   }
 
   public void setTransactionVerification(boolean transactionVerification) {
@@ -101,33 +145,15 @@ public class TransactionDetailActivityViewModel
     }
   }
 
-  private void saveData() {
-    getCompositeSubscription().add(userRepository.updateTransactions(transactionObject)
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(getSubscriptionBuilder().builder().onNext(value -> {
-          getActivity().showToast("Successfully Updated");
-          getActivity().finish();
-        }).onError(e -> {
-          String errorMessage = "Something went wrong";
-          if (e instanceof Throwable) {
-            errorMessage = StringUtils.isNotBlank(e.getMessage()) ? e.getMessage() : errorMessage;
-          }
-          getActivity().showError(errorMessage);
-        }).build()));
+  public Drawable getCategory() {
+    return category;
   }
 
-  public void fetchData() {
-    getCompositeSubscription().add(userRepository.getTransactions()
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(getSubscriptionBuilder().builder().onNext(value -> {
-          transactionObject = (TransactionObject) value;
-          updateExpense();
-        }).onError(error -> {
-          error.printStackTrace();
-        }).setUnsubscribeOnComplete(true).build()));
+  public int getState() {
+    return state;
   }
 
-  public void retry() {
-    saveData();
+  public String getTime() {
+    return time;
   }
 }
